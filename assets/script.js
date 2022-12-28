0e62b46b5ff7a042f6706354ae265f2676489605
joemeetjoe <joemeetjoe@gmail.com>
2022-12-27 21:40:58 -0500

    added basic structure of starting page and began to construct the arrays containing the questions and answers. styled the starting page.

---
+++ assets/script.js
@@ -0,0 +1,93 @@
+const startQuizButton = document.getElementById("start-quiz-button");
+
+const questionOne = {
+    question: "Commonly used data types DO NOT include",
+    answerOne: "strings",
+    answerTwo: "booleans",
+    answerThree: "alerts",
+    answerFour: "numbers"
+};
+
+const questionTwo = {
+    question: "Arrays in JavaScript can be used to store _________.",
+    answerOne: "numbers and strings",
+    answerTwo: "other arrays",
+    answerThree: "booleans",
+    answerFour: "all of the above"
+};
+
+const questionThree = {
+    question: "Commonly used data types do not include",
+    answerOne: "strings",
+    answerTwo: "booleans",
+    answerThree: "alerts",
+    answerFour: "numbers"
+};
+
+const questionFour = {
+    question: "String values must be enclosed within ______ when being assigned to variables.",
+    answerOne: "commas",
+    answerTwo: "curly brackets",
+    answerThree: "quotes",
+    answerFour: "parenthesis"
+};
+
+const questionFive = {
+    question: "A very useful tool used during development and debugging for printint content to the debugger is:",
+    answerOne: "JavaScript",
+    answerTwo: "terminal/bash",
+    answerThree: "for loops",
+    answerFour: "console.log"
+};
+
+const questionSix = {
+    question: "The condition in an if/else statement is enclosed with _______.",
+    answerOne: "quotes",
+    answerTwo: "curly brackets",
+    answerThree: "parenthesis",
+    answerFour: "square brackets"
+};
+
+const questionArray = [questionOne, questionTwo, questionThree, questionFour];
+
+
+
+
+function quizGame () {
+    for (var i=0; i < questionArray; i ++){
+        for 
+    }
+}
+
+startQuizButton.addEventListener("click", quizGame);
+
+
+
+
+
+
+
+// todo: make a button to start the game.
+
+// once button is hit, run code to re write the html and show the quiz information
+
+// create a timer that is counting down from 100.
+
+// find a way to verify bulleted list and make each bulleted list into buttons
+
+// if user hits the wrong button, it subtracts values from the timer and gives the user feedback "right or wrong"
+
+// once the game is done, give them a chance to type in their name and submit a high score. 
+
+// be able to save the high score for being able to look up later. 
+
+// put up a link to the high score page on the main page
+
+
+
+// base html
+// hit button 
+// for in or for each loop that looped the array
+// question { question 1: "blah blah blah"}
+// make a timer
+// 
